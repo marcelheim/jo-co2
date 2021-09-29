23ffdc407f30e45b862a1b9cd5569fc094e3f1d1
Marcel Heim <heim.marcel@outlook.com>
2021-09-29 08:57:20 +0200

    Initial Tooling

---
+++ server/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
