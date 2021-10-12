e066bad5bd8363e3b270b116c418c1b6615741ee
Marcel Heim <heim.marcel@outlook.com>
2021-10-12 13:26:08 +0200

    Add Auth0 login

--- server/src/routes/index.svelte
+++ server/src/routes/index.svelte
@@ -1,8 +1,21 @@
 <script>
+	import { stores } from '@sapper/app';
+	const { session } = stores();
 </script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Home</title>
 </svelte:head>
+  
+{#if $session.isAuthenticated}
+<div>
+	Signed in as <strong>{$session.user.name}</strong>
+	| <a href="/logout">Sign out</a>
+</div>
+{:else}
+<a href="/login">Sign In</a>
+{/if}
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<pre>
+{JSON.stringify($session, null, 2)}
+</pre>
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,47 @@
 import sirv from 'sirv';
-import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+const express = require('express')
+const { auth } = require('express-openid-connect');
+require('dotenv').config({
+	path: '../.env'
+})
 
-const { PORT, NODE_ENV } = process.env;
+const { 
+	PORT,
+	NODE_ENV,
+	AUTH0_BASE_URL,
+	AUTH0_CLIENT_ID,
+	AUTH0_ISSUER_BASE_URL,
+	AUTH0_CLIENT_SECRET
+} = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	baseURL: AUTH0_BASE_URL,
+	clientID: AUTH0_CLIENT_ID,
+	issuerBaseURL: AUTH0_ISSUER_BASE_URL,
+	secret: AUTH0_CLIENT_SECRET,
+};
+
+const app = express();
+
+app.use(compression({ threshold: 0 }));
+app.use(sirv('static', { dev }));
+app.use(auth(config));
+app.use((req, res, next) => {
+	return sapper.middleware({
+		session: () => {
+			return {
+				isAuthenticated: req.oidc.isAuthenticated(),
+				user: req.oidc.user,
+			};
+		}
+	})(req, res, next)
+});
+
+app.listen(PORT, (err) => {
+	if(err) console.log(err)
+})
