2be6c0eef0771d5dc0cba814c1e64a29c5604f19
Marcel Heim <heim.marcel@outlook.com>
2021-10-13 08:09:22 +0200

    Add dashboard files

--- server/src/components/Nav.svelte
+++ server/src/components/Nav.svelte
@@ -2,9 +2,10 @@
 	export let segment;
 </script>
 
-<nav>
-	<ul>
+<nav class="flex">
+	<ul class="flex justify-between w-full">
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+		<li><a aria-current="{segment === 'dashboard' ? 'page' : undefined}" href="dashboard">dashboard</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
 	</ul>
 </nav>
---
+++ server/src/routes/dashboard/[slug].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    export async function preload(page, session) {
+		const { slug } = page.params;
+		return { slug };
+	}
+</script>
+<script>
+    export let slug
+    if (typeof window !== 'undefined') {
+	    // client-only code here
+    }
+</script>
+Einen sensor <br/>
+Slug: {slug}
