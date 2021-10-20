92699814dc20a11836297625d69f4800c5dc77f6
Maxi Resch <maxi1232015@outlook.de>
2021-10-20 07:58:23 +0200

    Sensoren einlesen

---
+++ dataclient/DHT-11 + MHZ-19.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import dht11
+import mh_z19
+
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 4
+instance = dht11.DHT11(pin = 4)
+result = instance.read()
+
+if result.is_valid():
+    print("Temperature: %-3.1f C" % result.temperature)
+    print("Humidity: %-3.1f %%" % result.humidity)
+else:
+    print("Error: %d" % result.error_code)
+
+print(mh_z19.read_all())
+
