38712430bd58b8c80ea769452fb744165b771fc4
Marcel Heim <heim.marcel@outlook.com>
2021-11-10 09:14:33 +0100

    Add read api

--- server/src/lib/influxdb.js
+++ server/src/lib/influxdb.js
@@ -21,10 +21,10 @@ const write = async (dataClientId, data) => {
         .uintField('value', data.co2.value)
         .stringField('unit', data.co2.unit))
     if(data.temperature != null)  dataPoints.push(new Point('temperature')
-        .uintField('value', data.temperature.value)
+        .floatField('value', data.temperature.value)
         .stringField('unit', data.temperature.unit))
     if(data.humidity != null)  dataPoints.push(new Point('humidity')
-        .uintField('value', data.humidity.value)
+        .floatField('value', data.humidity.value)
         .stringField('unit', data.humidity.unit))
 
     writeApi.writePoints(dataPoints)
@@ -35,45 +35,4 @@ const write = async (dataClientId, data) => {
             console.error(e)
         })
 }
-
-
-const read = async (dataClientId) => {
-    const {
-        INFLUXDB_ORG,
-        INFLUXDB_BUCKET,
-        INFLUXDB_ADMIN_TOKEN,
-        INFLUXDB_URL
-    } = process.env
-
-    const queryApi = new InfluxDB({
-        url: INFLUXDB_URL,
-        token: INFLUXDB_ADMIN_TOKEN
-    }).getQueryApi(INFLUXDB_ORG)
-
-    const fluxQuery = `from(bucket: "${INFLUXDB_BUCKET}")
-        |> range(start: -3h)
-        |> filter(fn: (r) => r["0"] == "${dataClientId}")
-        |> filter(fn: (r) => r["_field"] == "value")`
-
-    let data = {}
-
-    const fluxObserver = {
-        next(row, tableMeta) {
-            const o = tableMeta.toObject(row)
-            if(!data[o._time]) data[o._time] = {}
-            data[o._time][o._measurement] = o._value
-        },
-        error(error) {
-            console.error(error)
-            console.log('\nFinished ERROR')
-        },
-        complete() {
-            console.log('\nFinished SUCCESS')
-            console.log(data)
-        }
-    }
-
-    queryApi.queryRows(fluxQuery, fluxObserver)
-}
-
-export {read, write}
+export {read}
--- server/src/routes/api/sensordata/[slug].js
+++ server/src/routes/api/sensordata/[slug].js
@@ -1,4 +1,5 @@
 import * as db from '../../../lib/influxdb'
+import { InfluxDB } from '@influxdata/influxdb-client'
 
 const post = async (req, res, next) => {
     const { slug } = req.params
@@ -14,8 +15,41 @@ const post = async (req, res, next) => {
 const get = async (req, res, next) => {
     const { slug } = req.params
     if (true) {
-        await db.read(slug)
-		res.end();
+		const {
+			INFLUXDB_ORG,
+			INFLUXDB_BUCKET,
+			INFLUXDB_ADMIN_TOKEN,
+			INFLUXDB_URL
+		} = process.env
+	
+		const queryApi = new InfluxDB({
+			url: INFLUXDB_URL,
+			token: INFLUXDB_ADMIN_TOKEN
+		}).getQueryApi(INFLUXDB_ORG)
+	
+		const fluxQuery = `from(bucket: "${INFLUXDB_BUCKET}")
+			|> range(start: -3h)
+			|> filter(fn: (r) => r["0"] == "${slug}")
+			|> filter(fn: (r) => r["_field"] == "value")`
+	
+		let data = {}
+		
+		const fluxObserver = {
+			next(row, tableMeta) {
+				const o = tableMeta.toObject(row)
+				if(!data[o._time]) data[o._time] = {}
+				data[o._time][o._measurement] = o._value
+			},
+			error(error) {
+				console.error(error)
+				res.end()
+			},
+			complete() {
+				res.end(JSON.stringify(data))
+			}
+		}
+	
+		queryApi.queryRows(fluxQuery, fluxObserver)
 	} else {
 		next()
 	}
